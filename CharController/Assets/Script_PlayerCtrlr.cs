21df40b0020a03054cae6f2fc979a0ef9a1f44a8
lucasdresch <49284431+lucasdresch@users.noreply.github.com>
2023-11-09 14:52:34 -0300

    Criando script do player
    - Criando controles de animação (Walk e Run)

---
+++ CharController/Assets/Script_PlayerCtrlr.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script_PlayerCtrlr : MonoBehaviour
+{
+    public Animator PlayerAnimator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerAnimator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftShift)) {
+            PlayerAnimator.SetFloat("AnimatorVel", Input.GetAxis("Vertical"));
+        } else {
+            PlayerAnimator.SetFloat("AnimatorVel", Input.GetAxis("Vertical") / 2.0f);
+        }
+    }
+}
