793bcfd1ec59bccd8bb52e7c2d60c78b3146c879
lucasdresch <49284431+lucasdresch@users.noreply.github.com>
2023-11-09 18:40:28 -0300

    Script do Player (3)
    - Organizando o código
    - Preparando e movimentando o Player

--- CharController/Assets/Script_PlayerCtrlr.cs
+++ CharController/Assets/Script_PlayerCtrlr.cs
@@ -5,27 +5,45 @@ using UnityEngine;
 public class Script_PlayerCtrlr : MonoBehaviour
 {
     private Animator PlayerAnimator;
+    private CharacterController CharCtrlr;
+    public float VelWalk;
+    public float VelRun;
+    private float VelocityCtrlr;
+    public float MoveForward;
+    public float InputForward;
+    public float InputRotate;
+    private Vector3 PlayerMovement;
 
     // Start is called before the first frame update
     void Start()
     {
         PlayerAnimator = GetComponent<Animator>();
+        CharCtrlr = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
-
-
+        InputMovements();
         AnimationsCtrlrKeys();
+        PlayerMovement = InputForward * transform.TransformDirection(Vector3.forward) * VelocityCtrlr;
+        CharCtrlr.Move(PlayerMovement * Time.deltaTime);
+
+    }
+
+    void InputMovements() {
+        InputForward = Input.GetAxis("Vertical");
+        InputRotate = Input.GetAxis("Horizontal");
+        
     }
 
     void AnimationsCtrlrKeys() {
         if (Input.GetKey(KeyCode.LeftShift)) {
-            PlayerAnimator.SetFloat("AnimatorVel", Input.GetAxis("Vertical"));
+            PlayerAnimator.SetFloat("AnimatorVel", InputForward);
+            VelocityCtrlr = VelRun;
         } else {
-            PlayerAnimator.SetFloat("AnimatorVel", Input.GetAxis("Vertical") / 2.0f);
+            PlayerAnimator.SetFloat("AnimatorVel", InputForward / 2.0f);
+            VelocityCtrlr = VelWalk;
         }
 
         if (Input.GetKeyDown(KeyCode.E)) {
